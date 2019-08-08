9a672bc35eb7a18b21631971cbc5be8af0c65884
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 07:32:09 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,5 @@
-def square_array(array)
-  # your code here
+def square_array(array, numbers = "1", "4", "9")
+when 'should square the elements in an array when added to the line' do
+  square_array = numbers
+
 end
